f7970ce5c6c6729b3693cb322e3981f753c06bc0
okkim90 <rodudtnf18@naver.com>
2021-12-19 22:06:31 +0900

    메인 작업

--- js/common.js
+++ js/common.js
@@ -1,8 +1,31 @@
 $(function(){
+    $(window).on('load resize scroll',function(){
+        if($(this).scrollTop() > 100){
+            $('.header').addClass('on');
+        }else{
+            $('.header').removeClass('on');
+        }
+    });
+
+    $('.nav_btn').on('click',function(){
+        if($(this).hasClass('on')){
+            $(this).removeClass('on');
+            $('.gnb').removeClass('on');
+        }else{
+            $(this).addClass('on');
+            $('.gnb').addClass('on');
+        }
+        
+    });
+
+
+
     setWinH()
     $(window).on('load resize',function(){
         setWinH();
     });
+
+    
 });
 
 function setWinH(){
